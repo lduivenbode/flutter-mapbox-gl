ac85785b2432b19cb58ed54af2635673de6e9622
Nathan Hamblen <nathan@transit.app>
2021-03-18 22:25:48 -0400

    User CAFrames to animate dot size changes
    Back to core animation because it's apparently the only recommended way
    to animate view contents. It doesn't seem to be the cause of the memory
    issues, especially not if animation is disabled for most updates.

--- ios/Classes/CustomUserLocationAnnotationView.swift
+++ ios/Classes/CustomUserLocationAnnotationView.swift
@@ -6,25 +6,37 @@ class CustomUserLocationAnnotationView: MGLUserLocationAnnotationView {
   let maxDotSize: CGFloat
   var dotSize: CGFloat
 
-  let arrowMeters: CGFloat = 4
+  let arrowMeters: CGFloat = 6
   let minArrowSize: CGFloat = 12
-  let maxArrowSize: CGFloat = 28
   let smallHitTestLayer: CALayer
   var arrowRotation: CGFloat?
-  var arrowSize: CGFloat
+  var arrowScale: CGFloat
+
+  let dotLayer: CALayer
+  let arrowLayer: CAShapeLayer
 
   let lineWidth: CGFloat = 2
 
   init() {
     let size: CGFloat = 200
-    dotSize = minDotSize
+    dotSize = 0.0
     maxDotSize = size - lineWidth
-    arrowSize = minArrowSize
+    arrowScale = 1.0
     smallHitTestLayer = CALayer()
     let hitTestOffset = size / 2 - minDotSize / 2
     smallHitTestLayer.frame = CGRect(x: hitTestOffset, y: hitTestOffset, width: minDotSize, height: minDotSize)
+    dotLayer = CALayer()
+    arrowLayer = CAShapeLayer()
+
     super.init(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: size, height: size)))
-    backgroundColor = UIColor.clear
+    isOpaque = false
+
+    dotLayer.borderColor = UIColor.white.cgColor
+    layer.addSublayer(dotLayer)
+
+    arrowLayer.path = getArrowPath()
+    arrowLayer.fillColor = dotLayer.borderColor
+    layer.addSublayer(arrowLayer)
   }
 
   override var hitTestLayer: CALayer? {
@@ -41,26 +53,67 @@ class CustomUserLocationAnnotationView: MGLUserLocationAnnotationView {
       let accuracySize = CGFloat(accuracy) / mpp * 2.0
       let dotMetersSize = minDotMeters / mpp
       let newDotSize = min(maxDotSize, max(dotMetersSize, max(minDotSize, accuracySize)))
+
       if newDotSize != dotSize {
-        dotSize = newDotSize
-        setNeedsDisplay()
+        layoutDotLayer(newDotSize: newDotSize)
       }
 
-      let newArrowSize = min(maxArrowSize, max(minArrowSize, arrowMeters / mpp))
-      if newArrowSize != arrowSize {
-        arrowSize = newArrowSize
-        setNeedsDisplay()
-      }
-
-      if let heading = userLocation!.heading?.trueHeading {
+      if let heading = userLocation?.heading?.trueHeading {
         let newArrowRotation = -MGLRadiansFromDegrees(mv.direction - heading)
+        let newArrowScale = max(1.0, (arrowMeters / mpp) / minArrowSize)
 
-        if newArrowRotation != arrowRotation {
-          arrowRotation = newArrowRotation
-          setNeedsDisplay()
+        if newArrowScale != arrowScale || newArrowRotation != arrowRotation {
+          layoutArrowLayer(newArrowScale: newArrowScale, newArrowRotation: newArrowRotation)
         }
       }
     }
+  }
+
+  private func layoutDotLayer(newDotSize: CGFloat) {
+    CATransaction.begin()
+    CATransaction.setDisableActions(abs(newDotSize - dotSize) < 4)
+
+    dotSize = newDotSize
+    let dotOffset = frame.size.width / 2.0 - dotSize / 2.0
+    dotLayer.backgroundColor = tintColor.cgColor
+    dotLayer.frame = CGRect(x: dotOffset, y: dotOffset, width: dotSize, height: dotSize)
+    dotLayer.cornerRadius = dotSize / 2
+    dotLayer.borderWidth = dotSize / 14
+    dotLayer.opacity = Float(min(1.0, 0.2 + (maxDotSize - dotSize) / maxDotSize))
+
+    CATransaction.commit()
+  }
+
+  private func layoutArrowLayer(newArrowScale: CGFloat, newArrowRotation: CGFloat) {
+    CATransaction.begin()
+    CATransaction.setDisableActions(true)
+    arrowScale = newArrowScale
+    arrowRotation = newArrowRotation
+
+    arrowLayer.setAffineTransform(CGAffineTransform(scaleX: arrowScale, y: arrowScale).concatenating(CGAffineTransform.identity.translatedBy(x: bounds.width / 2, y: bounds.height / 2).rotated(by: newArrowRotation)))
+
+    CATransaction.commit()
+  }
+
+  private func getArrowPath() -> CGPath {
+    let max: CGFloat = minArrowSize / 2.0
+    let pad: CGFloat = 0.8
+
+    let top = CGPoint(x: 0.0, y: -max * 1.1)
+    let left = CGPoint(x: -max * pad, y: max)
+    let right = CGPoint(x: max * pad, y: max)
+    let center = CGPoint(x: 0.0, y: max * 0.4)
+
+    let radius: CGFloat = max * 0.1
+
+    let path = CGMutablePath()
+    path.move(to: center)
+    path.addArc(tangent1End: left, tangent2End: top, radius: radius)
+    path.addArc(tangent1End: top, tangent2End: right, radius: radius)
+    path.addArc(tangent1End: right, tangent2End: center, radius: radius)
+    path.closeSubpath()
+
+    return path
   }
 
   private func metersPerPoint() -> CGFloat? {
@@ -68,53 +121,6 @@ class CustomUserLocationAnnotationView: MGLUserLocationAnnotationView {
       return CGFloat(mv.metersPerPoint(atLatitude: coord.latitude))
     } else {
       return nil
-    }
-  }
-
-  override func draw(_: CGRect) {
-    if let context = UIGraphicsGetCurrentContext() {
-      drawDot(context)
-      drawArrow(context)
-    }
-  }
-
-  private func drawDot(_ context: CGContext) {
-    let dotOffset = frame.size.width / 2.0 - dotSize / 2.0
-    let circleRect = CGRect(x: dotOffset, y: dotOffset, width: dotSize, height: dotSize)
-    context.setAlpha(min(1.0, 0.2 + (maxDotSize - dotSize) / maxDotSize))
-    context.setFillColor(super.tintColor.cgColor)
-    context.setStrokeColor(UIColor.white.cgColor)
-    context.setLineWidth(lineWidth)
-    context.fillEllipse(in: circleRect)
-    context.strokeEllipse(in: circleRect)
-    context.setAlpha(1.0)
-  }
-
-  // Calculate the vector path for an arrow, for use in a shape layer.
-  private func drawArrow(_ context: CGContext) {
-    if let rotation = arrowRotation {
-      let max: CGFloat = arrowSize / 2.0
-      let offset: CGFloat = frame.size.width / 2.0
-      let pad: CGFloat = 0.8
-
-      let top = CGPoint(x: 0.0, y: -max * 1.1)
-      let left = CGPoint(x: -max * pad, y: max)
-      let right = CGPoint(x: max * pad, y: max)
-      let center = CGPoint(x: 0.0, y: max * 0.4)
-
-      let radius: CGFloat = max * 0.1
-
-      let path = CGMutablePath()
-      path.move(to: center)
-      path.addArc(tangent1End: left, tangent2End: top, radius: radius)
-      path.addArc(tangent1End: top, tangent2End: right, radius: radius)
-      path.addArc(tangent1End: right, tangent2End: center, radius: radius)
-      path.closeSubpath()
-
-      let bezierPath = UIBezierPath(cgPath: path)
-      bezierPath.apply(CGAffineTransform(rotationAngle: rotation).concatenating(CGAffineTransform(translationX: offset, y: offset)))
-      context.setFillColor(UIColor.white.cgColor)
-      bezierPath.fill()
     }
   }
 }
